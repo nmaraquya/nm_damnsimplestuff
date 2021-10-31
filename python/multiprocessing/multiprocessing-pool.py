f02c09d21f77a2ae9af408ff2d5d0d14d4730021
Nikolai Mareev <nikolai.mareev.gs@gmail.com>
2021-10-31 22:28:07 +0100

    added comments

--- python/multiprocessing/multiprocessing-pool.py
+++ python/multiprocessing/multiprocessing-pool.py
@@ -6,19 +6,6 @@ import time
 def squareIT(n):
     return n**2
 
-def squareNumber(ns):
-    result=[]
-    for i in(ns):
-        result.append(i**2)
-    return result
-
-# function to be mapped over
-def calculateParallelLISTS(lists, threads=2):
-    pool = Pool(threads)
-    results = pool.map(squareNumber, lists)
-    pool.close()
-    pool.join()
-    return results
 
 def calculateParallel(listIK, threads=2):
     pool = Pool(threads)
@@ -30,18 +17,6 @@ def calculateParallel(listIK, threads=2):
 if __name__ == "__main__":
 
     startedAt = time.time()
-    sublistdepth=250000
-    listoflists=[]
-    tmplist=[]
-    for i in range(10000000):
-        tmplist.append(i)
-        if len(tmplist)>=sublistdepth:
-            listoflists.append(tmplist)
-            tmplist=[]
-    
-    print("formed list of lists of numbers in "+str(round(time.time()-startedAt,2))+" secs")
-    startedAt = time.time()
-
 
     tmplist=[]
     for i in range(10000000):
@@ -56,14 +31,4 @@ if __name__ == "__main__":
 
     startedAt = time.time()
     pool_output = calculateParallel(tmplist,4)
-    print("4 thread done task in "+str(round(time.time()-startedAt,2))+" secs")
-
-    
-    startedAt = time.time()
-    pool_output = calculateParallelLISTS(listoflists,1)
-    print("1 thread done task in "+str(round(time.time()-startedAt,2))+" secs")
-
-
-    startedAt = time.time()
-    pool_output = calculateParallelLISTS(listoflists,4)
     print("4 thread done task in "+str(round(time.time()-startedAt,2))+" secs")
