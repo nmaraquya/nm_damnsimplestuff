13747255299e275070d804074b0435c39cac4f54
Nikolai Mareev <nikolai.mareev.gs@gmail.com>
2022-02-17 00:12:09 +0100

    001example changed to rtt

--- python/pyparser/plotparce_001.py
+++ python/pyparser/plotparce_001.py
@@ -2,13 +2,13 @@ import os
 import nmlogparcer_v5 as nm
 import matplotlib.pyplot as plt
 
-ifile = "data/iperf3/iperf_ger_json.json"
+ifile = "data/iperf3/iperf_fra_json.json"
 x, y = nm.parceIPERFJSON(
-    ifile, keyPattern="bits_per_second", streamID="0", zero=True, timeshift=10)
+    ifile, keyPattern="rtt", streamID="0", zero=True, timeshift=10)
 plt.figure("title")
 plt.suptitle("subtitle")
 
-plt.plot(x, y, label="bits_per_second")
+plt.plot(x, y, label="rtt")
 plt.grid(True)
 legend = plt.legend(loc="upper left", shadow=True)
 plt.show()
